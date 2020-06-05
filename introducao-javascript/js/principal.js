10794429cedf726fb6532d18b4313b8ab77cef6a
Luíza Bizoni <bizoni.luiza@gmail.com>
2020-06-04 21:38:32 -0300

    Exercícios aula 04

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -16,7 +16,7 @@ for (var i = 0; i < pacientes.length; i++) {
     var tdAltura = paciente.querySelector(".info-altura"); 
     var altura = tdAltura.textContent;
 
-    var tdImc = paciente.querySelector(".info-imc")
+    var tdImc = paciente.querySelector(".info-imc");
 
     // Valida se a altura e o peso são validos
     var pesoEhValido = true;
@@ -26,14 +26,14 @@ for (var i = 0; i < pacientes.length; i++) {
         
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido";
-        paciente.classList.add("paciente-invalido")
+        paciente.classList.add("paciente-invalido");
     }
 
     if (altura <= 0 || altura >= 3.00) {
         
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida";
-        paciente.classList.add("paciente-invalido")
+        paciente.classList.add("paciente-invalido");
     }
 
     if (alturaEhValida && pesoEhValido) {
@@ -45,3 +45,38 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 }
 
+// Clique no botão do formulário
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault(); //Previne o comportamento padrão, logo evita que a página seja recarregada ao clicar no botão
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+}) 
