c0a9889b1c99022a8b6004d2a1093d0678c52ddc
Luíza Bizoni <bizoni.luiza@gmail.com>
2020-06-05 16:27:54 -0300

    Exercícios aula 05

---
+++ introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,53 @@
+// Seleciona e troca o título
+var titulo = document.querySelector(".titulo");
+
+titulo.textContent = "Aparecida Nutricionista";
+
+// Seleciona dados dos pacientes para calcular o imc
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+    
+    var paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura"); 
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    // Valida se a altura e o peso são validos
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+            
+        // Calcula imc
+        var imc = calculaImc(peso, altura);
+        // Preenche a coluna imc com o resultado do cálculo acima
+        tdImc.textContent = imc;
+    }
+}
+
+function calculaImc(peso, altura) {
+    var imc = 0
+
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
