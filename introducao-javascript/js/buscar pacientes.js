f32655b84fd54e7df6785c4f4c5857ac5a031050
Luíza Bizoni <bizoni.luiza@gmail.com>
2020-06-09 20:05:51 -0300

    Exercícios aula 09

---
+++ introducao-javascript/js/buscar pacientes.js
@@ -0,0 +1,28 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function() {
+    
+    var xhr = new SMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function() {
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if(xhr.status == 200){
+            
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.respomseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(paciente => {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            var erroAjax = document.querySelector("#erro-ajax");
+            erroAjax.classList.remove("invisivel");
+        }
+    })
+
+    xhr.send();
+})
--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -9,8 +9,6 @@ botaoAdicionar.addEventListener("click", function(event) {
     
     var paciente = obtemPacienteDoFormulario(form);
 
-    var pacienteTr = montaTr(paciente);
-
     var erros = validaPaciente(paciente);
 
     if(erros.length > 0) {
@@ -24,14 +22,20 @@ botaoAdicionar.addEventListener("click", function(event) {
         return;
     }
 
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
+    adicionaPacienteNaTabela(paciente);
+    
     form.reset();
     var mensagensErro = document.querySelector("mensagens-erro");
     mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente) {
+
+    var pacienteTr = montaTr(paciente);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function obtemPacienteDoFormulario(form) {
 
