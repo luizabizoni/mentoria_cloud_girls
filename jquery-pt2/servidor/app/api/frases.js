4c1b7f6c2dfa27d2957024dfdddc31e39ada4993
Luíza Bizoni <bizoni.luiza@gmail.com>
2020-07-05 15:35:14 -0300

    Exercícios aula 06

--- jquery-pt2/servidor/app/api/frases.js
+++ jquery-pt2/servidor/app/api/frases.js
@@ -7,7 +7,7 @@ var frases = [
 	{_id: 3, texto:'Existem duas tarefas difíceis na Ciência da Computação: invalidação de cache e nomear as coisas.', tempo: 15 },
 	{_id: 4, texto:'Ciências da computação é tão sobre computadores quanto astronomia é sobre telescópios.', tempo: 15 },
 	{_id: 5, texto:'Na minha máquina funciona.', tempo: 5 },
-	{_id: 6, texto:'Hardware é o que você chuta, software é o que você chinga.', tempo: 12 },
+	{_id: 6, texto:'Hardware é o que você chuta, software é o que você xinga.', tempo: 12 },
 	{_id: 7, texto:'Software em funcionamento mais que documentação abrangente.', tempo: 10 },
 	{_id: 8, texto:'Iterar é humano, recursão é divino.', tempo: 7},
 	{_id: 9, texto:'Existem três jeitos de desenvolver software. O jeito certo, o jeito errado e o meu jeito, que é igual o jeito errado só que mais rápido.', tempo: 20},
