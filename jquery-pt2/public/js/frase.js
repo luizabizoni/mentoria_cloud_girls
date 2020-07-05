0c31a3a4bc663ac12d70e26a6b972f7b89db311d
Luíza Bizoni <bizoni.luiza@gmail.com>
2020-07-05 15:29:22 -0300

    Exercícios aula 05

--- jquery-pt2/public/js/frase.js
+++ jquery-pt2/public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
 	$("#spinner").toggle();
@@ -21,4 +22,36 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+
+function buscaFrase() {
+
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+
+    //criacao do objeto JS que guarda a id
+    var dados = {id : fraseId}; 
+
+    //passando objecto como segundo parametro
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+    },2000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+
+    console.log(data);
+
+    var frase = $(".frase");
+    frase.text(data.texto); //cuidado, texto com "o" no final 
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
