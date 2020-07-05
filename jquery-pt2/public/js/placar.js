291d7c02aad29a1020cafc071e7c64f4b181148a
Luíza Bizoni <bizoni.luiza@gmail.com>
2020-07-05 15:41:41 -0300

    Exercícios aula 07

--- jquery-pt2/public/js/placar.js
+++ jquery-pt2/public/js/placar.js
@@ -3,7 +3,7 @@ $("#botao-sync").click(sincronizaPlacar);
 
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Douglas"
+    var usuario = $("#usuarios").val();
     var numPalavras = $("#contador-palavras").text();
 
     var linha = novaLinha(usuario, numPalavras);
@@ -76,9 +76,16 @@ function sincronizaPlacar(){
             placar: placar
         };
 
-        $.post("http://localhost:3000/placar", dados, function(){
+        $.post("http://localhost:3000/placar", dados , function() {
             console.log("Placar sincronizado com sucesso");
-        });
+            $(".tooltip").tooltipster("open"); 
+        }).fail(function(){
+            $(".tooltip").tooltipster("open").tooltipster("content", "Falha ao sincronizar"); 
+        }).always(function(){ //novo
+            setTimeout(function() {
+            $(".tooltip").tooltipster("close"); 
+        }, 1200);
+});
 
     });
 }
