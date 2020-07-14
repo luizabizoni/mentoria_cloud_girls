999e711a938213b33d4b8c028b1233116d3d49a5
Luíza Bizoni <bizoni.luiza@gmail.com>
2020-07-14 20:49:40 -0300

    Exercícios aula 03

--- java-pt1/Programa.java
+++ java-pt1/Programa.java
@@ -1,7 +1,5 @@
-public class Programa{
-
-    public static void main(String[] args){
-
-        System.out.println("ola mundo");
-    }
+public class Programa {
+	public static void main(String[] args) {
+		System.out.println("ola mundo");
+	}
 }
